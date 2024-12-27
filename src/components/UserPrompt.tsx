e01e5692cb80d0878acaa14bc3631b51e172213e
shriramcs <shriramcs@gmail.com>
2024-12-27 13:10:07 +0100

    added logo

--- src/components/UserPrompt.tsx
+++ src/components/UserPrompt.tsx
@@ -29,25 +29,35 @@ const UserPrompt: React.FC<UserPromptProps> = ({ onStartGame }) => {
 
   return (
     <div className="user-prompt">
-      <h3>Enter Number of Players</h3>
-      <input
-        type="number"
-        min="1"
-        max="4"
-        value={numPlayers}
-        onChange={(e) => setNumPlayers(Number(e.target.value))}
-      />
-      <h3><label htmlFor="playerCount">Enter Player Names</label></h3>
-      {[...Array(numPlayers)].map((_, index) => (
+      <div className="logo">
+        <div className="logo-card logo-card1"></div>
+        <div className="logo-card logo-card2"></div>
+      </div>
+      <h2 className='header-name'>Match the pairs </h2>
+      <div>
+        <h3>Enter Number of Players</h3>
         <input
-          key={index}
-          type="text"
-          id="playerCount"
-          placeholder={`Player ${index + 1} Name`}
-          onChange={(e) => handleInputChange(index, e.target.value)}
+          type="number"
+          min="1"
+          max="4"
+          value={numPlayers}
+          onChange={(e) => setNumPlayers(Number(e.target.value))}
         />
-      ))}
-      <button onClick={handleStart}>Start Game</button>
+      </div>
+      
+      <section>
+        <h3><label htmlFor="playerCount">Enter Player Names</label></h3>
+        {[...Array(numPlayers)].map((_, index) => (
+          <input
+            key={index}
+            type="text"
+            id="playerCount"
+            placeholder={`Player ${index + 1} Name`}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+          />
+        ))}
+        <button onClick={handleStart}>Start Game</button>
+      </section>
     </div>
   );
 };
